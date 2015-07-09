71021f4e66b4084c74cc8cd65c550c8b9270e419
vingh <vinodmandala@gmail.com>
2015-07-09 14:02:00 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#this script takes input from ./tweet_input/tweets.txt
+#produces output to ./tweet_output/ft1.txt and ./tweet_output/ft2.txt
+
+python ./src/process_tweets.py
